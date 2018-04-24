213b5adb354ae705b7a6b335fe94fb8b15237dcd
hjaraba <hjaraba.secpro@gmail.com>
2018-04-24 16:37:19 +0200

    Revert "Merge branch 'master' into Hector"
    This reverts commit 9d855dc8feab05afdb0afedaeab9fcab15a7d236.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -164,9 +164,6 @@ public class PlayerController : MonoBehaviour {
             contadorPuntos = contadorPuntos + collision.GetComponent<Gem>().points;
             puntuacion.text = "Puntos: " + contadorPuntos;
             if (contadorPuntos >= puntosGanar) Win();
-=======
-            int puntos = collision.GetComponent<Gem>().Puntos();
->>>>>>> Stashed changes
             Destroy(collision.gameObject);
         }
 
