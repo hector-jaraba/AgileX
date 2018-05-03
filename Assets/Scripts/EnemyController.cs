2468894cfd943a1f1614838fbfd1917522651cda
silmarm6 <32105635+silmarm6@users.noreply.github.com>
2018-05-03 16:48:19 +0200

    creados los enemigos y con movimiento

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour {
+
+    public float speedEnemy = 1f;
+    public float maxSpeedEnemy = 1f;
+    private Rigidbody2D enemyRigidBody;
+
+    // Use this for initialization
+    void Start () {
+        enemyRigidBody = GetComponent<Rigidbody2D>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        enemyRigidBody.AddForce(Vector2.right * speedEnemy);
+        float limitedSpeed = Mathf.Clamp(enemyRigidBody.velocity.x, -maxSpeedEnemy, maxSpeedEnemy);
+        enemyRigidBody.velocity = new Vector2(limitedSpeed, enemyRigidBody.velocity.y);
+
+        if (enemyRigidBody.velocity.x > -0.01f && enemyRigidBody.velocity.x < 0.01f)
+        {
+            speedEnemy = -speedEnemy;
+            enemyRigidBody.velocity = new Vector2(speedEnemy, enemyRigidBody.velocity.y);
+        }
+
+        if (speedEnemy < 0)
+            transform.localScale = new Vector3(1f, 1f, 1f);
+        else if (speedEnemy > 0)
+            transform.localScale = new Vector3(-1f, 1f, 1f);
+    }
+}
