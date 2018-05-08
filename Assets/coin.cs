d81a5ad8782d9e112e98c1dadd647cb38639f425
David <dakohbod@gmail.com>
2018-05-08 18:18:45 +0200

    Corrección de errores

--- Assets/coin.cs
+++ Assets/coin.cs
@@ -5,16 +5,13 @@ using UnityEngine;
 public class coin : MonoBehaviour {
     public int points = 0;
 
-    private BoxCollider2D GameObjectCoin;
-    private CircleCollider2D GameObjectCoinCircle;
 
 
 
     // Use this for initialization
     void Start()
     {
-        GameObjectCoin = GetComponent<BoxCollider2D>();
-        GameObjectCoinCircle = GetComponent<CircleCollider2D>();
+
 
     }
 
@@ -24,16 +21,10 @@ public class coin : MonoBehaviour {
 
     }
 
-    private void OnCollisionEnter2D(Collision2D collision)
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.CompareTag("Player"))
-        {
-
-            transform.localScale = new Vector3(0.5f, 0.5f);
-            Destroy(GameObjectCoin);
-            Destroy(GameObjectCoinCircle);
-            new WaitForSeconds(0.4f);
-            transform.localScale = new Vector3(0.05f, 0.05f);
+        if (collision.tag == "Player"){
+            Destroy(this.gameObject);
         }
     }
 
