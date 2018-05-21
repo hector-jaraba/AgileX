33a517eb5a8b4ec5194f2b589d4d3a48486a903c
silmarm6 <32105635+silmarm6@users.noreply.github.com>
2018-05-21 20:20:59 +0200

    Añadido premios que recuperan la energia

---
+++ Assets/Scripts/PotionsEnergy.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PotionsEnergy : MonoBehaviour {
+
+    // Use this for initialization
+    private BarManager barraEnergia;
+    private int smallPotion = 20;
+    private int bigPotion = 50;
+ 
+    void Start () {
+        barraEnergia = GameObject.Find("BarManager").GetComponent<BarManager>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            if (this.tag == "smallPotion")
+                restoreSmallEnergy();
+            else
+                restoreBigEnergy();
+            Destroy(this.gameObject);
+        }
+    }
+
+  
+
+    void restoreSmallEnergy()
+    {
+        int maxEnergy = 100;
+        if (barraEnergia.getEnergy() <= maxEnergy - smallPotion)
+        {
+            barraEnergia.doEnergy(smallPotion);
+        }
+        else
+        {
+            barraEnergia.maxEnergy();
+        }
+    }
+
+    void restoreBigEnergy()
+    {
+        int maxEnergy = 100;
+        if (barraEnergia.getEnergy() <= maxEnergy - bigPotion)
+        {
+            barraEnergia.doEnergy(bigPotion);
+        }
+        else
+        {
+            barraEnergia.maxEnergy();
+        }
+    }
+}
