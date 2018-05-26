88bdd89d12aceb6d4d493acb805645f6587f1981
silmarm6 <32105635+silmarm6@users.noreply.github.com>
2018-05-26 23:14:32 +0200

    añadido InputField para el username

--- Assets/Scripts/Estado.cs
+++ Assets/Scripts/Estado.cs
@@ -1,18 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Estado : MonoBehaviour {
 
     public int mejoraArma { get; set; }
+    public string username;
+    public GameObject name;
+    
 	// Use this for initialization
 	void Start () {
         DontDestroyOnLoad(this);
-	}
+    }
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        username = name.GetComponent<InputField>().text;
+    }
 
 }
